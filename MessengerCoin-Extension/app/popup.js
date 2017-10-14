d154c8e16d79af55b426f36b98dfeddd86793bbd
Friedbaum <friedbaumer@gmail.com>
2017-10-14 15:32:04 +0200

    Now.

---
+++ MessengerCoin-Extension/app/popup.js
@@ -0,0 +1,20 @@
+
+
+document.write("Your Tripcode for /BIZ/&nsbp&nsbp&nsbp&nsbp&nsbp&nsbp");
+
+document.write("Your Tripcode for /POL/&nsbp&nsbp&nsbp&nsbp&nsbp");
+
+document.write("Your Tripcode for /g/&nsbp&nsbp&nsbp&nsbp&nsbp&nsbp");
+
+
+
+
+var tripcodepol;
+
+var tripcodebiz;
+
+var tripcodeg;
+
+
+
+
